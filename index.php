6a2719aee1755016f9d8a1088398b57131e50278
aldotkol <aldo.doffizi@gmail.com>
2020-03-05 11:22:38 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		<title>dcr</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="dcr.css" />
+		<link rel="stylesheet" href="dcr2.css" />
 		<link rel="stylesheet" href="jquery.mobile.icons.min.css" />
 		<link rel="stylesheet" href="jquery.mobile.structure-1.4.5.min.css" /> 
 		<script src="jquery-1.11.1.min.js"></script> 
