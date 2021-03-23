8c65b8f76fb9b4192b5a269cba947cde7273b3f4
Aldo <aldo.doffizi@datamanagementitalia.it>
2021-03-23 11:34:42 +0100

    Update test.php

--- test.php
+++ test.php
@@ -26,4 +26,19 @@ test_static(); //stamperà 1
 test_static(); //stamperà 2
 test_static(); //stamperà 3
 
+
+$eta_utenti = array(
+'Simone' => 29,
+'Josephine' => 30,
+'Giuseppe' => 23,
+'Renato' => 26,
+'Gabriele' => 24
+);
+$somma_eta = 0;
+foreach ($eta_utenti as $nome => $eta) {
+echo "L'utente " . $nome . " ha " . $eta . " anni\n";
+$somma_eta += $eta;
+}
+echo "La somma delle età degli utenti è: " . $somma_eta;
+
 ?>
